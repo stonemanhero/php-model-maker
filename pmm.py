8c7af0fb6d7d53b30cf1efa5ee2b0bcdfe684cba
Unknown <stonemanhero@gmail.com>
2017-02-06 00:15:04 +0100

    create_model function added

--- pmm.py
+++ pmm.py
@@ -15,6 +15,37 @@ def check_syntax(code):
 		print("Syntax error in configuration file.")
 		sys.exit()
 
+def create_model(name, attrs):
+	model_filename = "Class" + name + ".php" 
+	output_file = open(model_filename, 'w')
+
+	output_file.write("<?php\n")
+	output_file.write("\tclass " + name + "\n")
+	output_file.write("\t{\n")
+
+	for attr in attrs:
+		output_file.write("\t\tprivate $" + attr + "\n")
+
+	output_file.write("\n\t\t// setters\n")
+
+	for attr in attrs:
+		output_file.write("\t\tpublic function set_" + attr + "($new_" + attr + ")\n")
+		output_file.write("\t\t{\n")
+		output_file.write("\t\t\t$this->" + attr + " = $new_" + attr + ";\n")
+		output_file.write("\t\t}\n\n")
+
+	output_file.write("\t\t// getters\n")
+
+	for attr in attrs:
+		output_file.write("\t\tpublic function get_" + attr + "()\n")
+		output_file.write("\t\t{\n")
+		output_file.write("\t\t\treturn $this->" + attr + ";\n")
+		output_file.write("\t\t}\n\n")
+
+	output_file.write("\t}")
+
+	print "All done!"
+
 def main():
 	if not os.path.isfile("model.pmm"):
 		print("You need to create model.pmm (configuration file).")
@@ -33,6 +64,8 @@ def main():
 	model_attr = []
 	for attr in code[1:]:
 		model_attr.append(attr.strip('\n'))
+
+	create_model(model_name, model_attr)
 
 if __name__ == "__main__":
     main()
